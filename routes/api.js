9774af0d4c96a444ddf0310dcef2564ace2f8333
Philippe Luchansky <philuchansky@gmail.com>
2015-11-10 15:47:12 -0800

    individual api routes. Boom!

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,70 @@
 var express = require('express');
 var api = express.Router();
 
+var data = {    students: [        {name: "Alexander Juneja", email: "alexjuneja1@gmail.com"},        {name: "Andrew Kim", email: "andyjinkim@gmail.com"},        {name: "Brian Frager", email: "btfrager@gmail.com"},        {name: "Genny Langan", email: "cglangan78@gmail.com"},        {name: "Colin Chan", email: "clnsdabst@gmail.com"},        {name: "Crystal Murray", email: "crystalsmurray@gmail.com"},        {name: "Dan McQuillan", email: "djmcquillan76@gmail.com"},        {name: "Eddie Jung", email: "ejung716@gmail.com"},        {name: "Ellie Shahla", email: "ess9213@yahoo.com"},        {name: "Emma Gilhuly", email: "emmagilhuly@me.com"},        {name: "Eunice Chang", email: "omgunis@gmail.com"},        {name: "Ivy Wong", email: "ivykww@gmail.com"},        {name: "John Meredith", email: "johnm589@yahoo.com"},        {name: "Juan Ruelas", email: "jruel006@ucr.edu"},        {name: "Ken Carrington", email: "kcarring93@verizon.net"},        {name: "Kevin Williams", email: "kevin.williams@onidaprep.com"},        {name: "Kristen Gunn", email: "ohkristen@creativekdg.com"},        {name: "Lilly Ghassemieh", email: "lillygh@gmail.com"},        {name: "Nick Hendren", email: "nhendren7@gmail.com"},        {name: "Rich Donnellan", email: "rich.donnellan@gmail.com"},        {name: "Sam Hager", email: "sam.hager11@gmail.com"},        {name: "Sam Richardson", email: "samrichardson01@hotmail.com"},        {name: "Santiago Casar", email: "santiago@nowhereprod.com"},        {name: "Sean Esteva", email: "seangeleno@gmail.com"},        {name: "Sean Hall", email: "seanhall22@gmail.com"},        {name: "Seri Orfali", email: "seriorfali@gmail.com"},        {name: "Stefanie Koenig", email: "stefanie.l.koenig@gmail.com"},        {name: "Ted Shumway", email: "shumway.ted@gmail.com"},        {name: "Veronica Sykes", email: "vasykes@gmail.com"}    ]}
+
 api.get('/', function(req,res) {
-	res.json({message: "Our API's root route!"});
+	res.json(data);
 });
 
+api.get('/students', function(req,res){
+	res.json(data.students)
+});
+
+api.get('/students/:id', function(req,res) {
+	res.json(data.students[req.params.id])
+});
+
+api.post('/students', function(req,res){
+	data.students.push({name: "Some New Name", email: "newemail@gmail.com"});
+
+	var updatedResponse = {
+		message: "New Student Added!",
+		students: data.students
+	}
+
+	res.json(updatedResponse);
+});
+
+api.delete('/students/:id', function(req,res) {
+	data.students.splice(req.params.id,1);
+	res.json(data.students);
+})
+
+//MAKE A PUT REQUEST HANDLER FOR A SPECIFIC STUDENT, TO UPDATE THEIR INFO TO SOMETHING NEW
+api.put('/students/:id', function(req,res){
+	//data.students.splice(req.params.id,1, {name: "New Name", email: "newemail@gmail.com" });
+
+	data.students[req.params.id].name = "New Name";
+	data.students[req.params.id].email = "newemail@gmail.com";
+
+
+	res.json(data.students)
+})
+
+
+
+
+
+
 module.exports = api
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
