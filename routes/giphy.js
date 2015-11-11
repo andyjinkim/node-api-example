6218018fd65f8dd5b26118da91a197242c104063
Andy Kim <andyjinkim@gmail.com>
2015-11-10 16:49:44 -0800

    random functionality added

--- routes/giphy.js
+++ routes/giphy.js
@@ -1,17 +1,15 @@
-var express = require('express');
-var giphy = express.Router();
-var request = require('request');
-
-giphy.get('/:query', function(req, res){
-	var searchString = "http://api.giphy.com/v1/gifs/search?q=" + req.params.query + "&api_key=dc6zaTOxFJmzC";
-	request(searchString, function(error, response, body){
-		var imgsrc = JSON.parse(body).data[0].images.fixed_height.url;
-		res.send('<img src="' + imgsrc + '">');
+var express         = require('express')
+var giphy           = express.Router()
+var request         = require('request')
 
 
-	})
-});
+giphy.get('/:query', function(req,res){
+  var searchString = "http://api.giphy.com/v1/gifs/search?q=" + req.params.query + "&api_key=dc6zaTOxFJmzC"
+  request(searchString, function(error, response, body){
+    var imgsrc = JSON.parse(body).data[Math.floor((Math.random()*(JSON.parse(body).data).length))].images.fixed_height.url
+    res.send('<img src="' + imgsrc + '">')
+      })
+})
 
 
-
-module.exports = giphy;
+module.exports = giphy
