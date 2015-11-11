96f9ec7124654710e5adcbadde18fe3e3dd70403
Philippe Luchansky <philuchansky@gmail.com>
2015-11-10 16:43:30 -0800

    giphy route with request npm

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var apiRoutes = require('./routes/api.js');
+var giphyRoutes = require('./routes/giphy.js');
 
 app.get('/', function(req, res){
 	res.json({message: "Welcome To My App!"})
 })
 
-
 app.use('/api', apiRoutes);
+app.use('/giphy', giphyRoutes);
 
 app.listen(3000, function(){
 	console.log("Server running. Magic will happen. Sparks will fly!");
--- routes/giphy.js
+++ routes/giphy.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var giphy = express.Router();
+var request = require('request');
+
+giphy.get('/:query', function(req, res){
+	var searchString = "http://api.giphy.com/v1/gifs/search?q=" + req.params.query + "&api_key=dc6zaTOxFJmzC";
+	request(searchString, function(error, response, body){
+		var imgsrc = JSON.parse(body).data[0].images.fixed_height.url;
+		res.send('<img src="' + imgsrc + '">');
 
 
+	})
+});
 
 
 
